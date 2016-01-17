05815df614d521dba9be5832730aa07b8f89d5dd
eleanorstrib <eleanor@sloan.mit.edu>
2016-01-16 21:48:43 -0800

    added most outlets for exiting storyboard elements

--- flick_finder/MainController.swift
+++ flick_finder/MainController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class MainController: UIViewController {
-
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var imageResult: UIImageView!
+    
+    @IBOutlet weak var textSearchField: UITextField!
+    
+    @IBOutlet weak var textSearchSubmit: UIButton!
+    
+    @IBOutlet weak var latSearchField: UITextField!
+    
+    @IBOutlet weak var latSearchLabel: UILabel!
+    
+    @IBOutlet weak var longSearchField: UITextField!
+    @IBOutlet weak var longSearchLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(red: 0/255, green: 141/255, blue: 163/255, alpha: 1.0)
